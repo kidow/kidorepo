439487c306cce37c898e33ae1c9bf8708e1ad092
kidow <wcgo2ling@gmail.com>
2023-07-04 16:24:12 +0900

    feat: editorjs 적용. 내용 변경 시 자동으로 로컬스토리지에 저장하고 새로고침 시 붙여넣기

--- apps/memo/src/App.tsx
+++ apps/memo/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect } from 'react'
+import EditorJS, { type OutputData } from '@editorjs/editorjs'
+
 function App() {
+  useEffect(() => {
+    const editor = new EditorJS({
+      holder: 'editorjs',
+      placeholder: '아무 내용이나 입력하세요...',
+      autofocus: true,
+      inlineToolbar: true,
+      onChange: async () => {
+        const content = await editor.save()
+        window.localStorage.setItem('content', JSON.stringify(content))
+      },
+      data:
+        (JSON.parse(
+          window.localStorage.getItem('content') as string
+        ) as OutputData) || undefined
+    })
+  }, [])
   return (
     <>
-      <div className="container mx-auto">asd</div>
+      <div id="editorjs" />
     </>
   )
 }
