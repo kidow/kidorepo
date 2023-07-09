c566bfaba0b1716afeb13a149ad1d9bd5e26ce51
kidow <wcgo2ling@gmail.com>
2023-07-09 20:54:22 +0900

    feat(admin,archive): types 폴더 적용

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -3,14 +3,10 @@ import { type Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Admin | Kidow'
+  title: 'Admin | kidow'
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: ReactProps) {
   return (
     <html lang="en">
       <body>{children}</body>
--- apps/archive/app/layout.tsx
+++ apps/archive/app/layout.tsx
@@ -3,14 +3,10 @@ import { type Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Archive | Kidow'
+  title: 'Archive | kidow'
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: ReactProps) {
   return (
     <html lang="en">
       <body>{children}</body>
