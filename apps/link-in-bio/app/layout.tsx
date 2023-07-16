ff358691df6d782951154c7617dc4d19ae13263c
kidow <wcgo2ling@gmail.com>
2023-07-16 21:02:42 +0900

    feat(link-in-bio): gtm 적용

--- apps/link-in-bio/app/layout.tsx
+++ apps/link-in-bio/app/layout.tsx
@@ -10,6 +10,7 @@ import Header from '@/components/Header'
 import './globals.css'
 
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ['latin']
@@ -19,6 +20,16 @@ export default function RootLayout({ children }: ReactProps) {
   return (
     <html lang="ko">
       <body className={inter.className}>
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TFHQLMX');`
+          }}
+        />
         <main className="flex min-h-screen flex-col items-center justify-center">
           <div className="flex min-h-screen w-full max-w-[1728px] flex-col">
             <div className="relative flex min-h-screen w-full flex-1 flex-col items-center">
