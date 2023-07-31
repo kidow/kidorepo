e36c8052045e5fb3ab1e8ae2c2f897f56bdb3d7e
kidow <wcgo2ling@gmail.com>
2023-07-31 11:13:23 +0900

    feat(link-in-bio): post 디자인 보정

--- apps/link-in-bio/components/Post.tsx
+++ apps/link-in-bio/components/Post.tsx
@@ -9,7 +9,7 @@ interface Props extends BlogItem {}
 export default function Post(page: Props) {
   return (
     <li>
-      <article className="group relative rounded-[10px] border">
+      <article className="group relative overflow-hidden rounded-[10px] border">
         <div className="overflow-hidden">
           <Image
             src={page.cover?.external?.url}
@@ -22,8 +22,10 @@ export default function Post(page: Props) {
           />
         </div>
         <div className="space-y-2 p-5 xl:p-6">
-          <h2 className="after:bg-primary relative inline-block overflow-hidden text-2xl font-extrabold text-slate-900 after:absolute after:bottom-1 after:left-0 after:h-1.5 after:w-full after:origin-bottom-right after:-translate-x-full after:opacity-50 after:transition-transform after:duration-150 after:content-[''] after:group-hover:translate-x-0">
-            {page.properties?.제목?.title[0]?.plain_text}
+          <h2 className="overflow-hidden text-2xl font-extrabold text-slate-900 xl:h-16">
+            <span className="after:bg-primary relative block after:absolute after:bottom-1 after:left-0 after:h-1.5 after:w-full after:origin-bottom-right after:-translate-x-full after:opacity-50 after:transition-transform after:duration-150 after:content-[''] after:group-hover:translate-x-0">
+              {page.properties?.제목?.title[0]?.plain_text}
+            </span>
           </h2>
           <p className="line-clamp-2 text-slate-500">
             {page.properties?.설명?.rich_text[0]?.plain_text}
