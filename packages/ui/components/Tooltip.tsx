8faaa7a69945c6cb6a8f6c9b6165e42b700ff951
kidow <wcgo2ling@gmail.com>
2023-08-01 18:29:11 +0900

    feat(link-in-bio): cva로 Tooltip 컴포넌트 재설계

--- packages/ui/components/Tooltip.tsx
+++ packages/ui/components/Tooltip.tsx
@@ -1,16 +1,66 @@
 'use client'
 
-import { memo } from 'react'
-import type { FC } from 'react'
-import classnames from 'classnames'
-import type { Argument } from 'classnames'
+import { ButtonHTMLAttributes, memo, type FC } from 'react'
+import { cva, type VariantProps } from 'class-variance-authority'
 
-export interface Props extends ReactProps {
+export interface Props
+  extends VariantProps<typeof variants>,
+    ButtonHTMLAttributes<HTMLButtonElement> {
   content: string
-  arrow?: boolean
-  className?: Argument
-  position?: 'top' | 'right' | 'bottom' | 'left'
 }
+
+const variants = cva(
+  'relative inline-block text-center before:pointer-events-none before:absolute before:z-[9999] before:w-max before:max-w-xs before:rounded before:bg-neutral-800 before:px-2 before:py-1 before:text-xs before:text-neutral-50 before:opacity-0 before:delay-100 before:duration-200 before:ease-in-out before:content-[attr(data-tip)] hover:before:opacity-100 hover:before:delay-75 dark:before:bg-neutral-700',
+  {
+    variants: {
+      position: {
+        top: 'before:bottom-[calc(100%+4px)] before:top-auto before:left-1/2 before:-translate-x-1/2',
+        right:
+          'before:left-[calc(100%+5px)] before:top-1/2 before:-translate-y-1/2',
+        bottom:
+          'before:top-[calc(100%+4px)] before:left-1/2 before:-translate-x-1/2',
+        left: 'before:left-auto before:right-[calc(100%+5px)] before:top-1/2 before:-translate-y-1/2'
+      },
+      arrow: {
+        true: 'after:absolute after:z-[9999] after:block after:h-0 after:w-0 after:border-[3px] after:border-transparent after:opacity-0 after:delay-100 after:duration-200 after:ease-in-out after:content-[""] hover:after:opacity-100 hover:after:delay-75'
+      }
+    },
+    compoundVariants: [
+      {
+        position: ['top', 'right', 'bottom'],
+        className: 'before:right-auto'
+      },
+      {
+        position: 'top',
+        arrow: true,
+        className:
+          'after:bottom-[calc(100%-2px)] after:top-auto after:border-t-neutral-800 dark:after:border-t-neutral-700 after:left-1/2 after:-translate-x-1/2 after:right-auto'
+      },
+      {
+        position: 'right',
+        arrow: true,
+        className:
+          'after:left-[calc(100%-1px)] after:border-r-neutral-800 dark:after:border-r-neutral-700 after:right-auto after:top-1/2 after:-translate-y-1/2 after:bottom-auto'
+      },
+      {
+        position: 'bottom',
+        arrow: true,
+        className:
+          'after:top-[calc(100%-2px)] after:border-b-neutral-800 dark:after:border-b-neutral-700 after:left-1/2 after:-translate-x-1/2 after:right-auto after:bottom-auto'
+      },
+      {
+        position: 'left',
+        arrow: true,
+        className:
+          'after:left-auto after:right-[calc(100%-1px)] after:border-l-neutral-800 dark:after:border-l-neutral-700 after:top-1/2 after:-translate-y-1/2 after:bottom-auto'
+      }
+    ],
+    defaultVariants: {
+      position: 'top',
+      arrow: true
+    }
+  }
+)
 
 const Tooltip: FC<Props> = ({
   children,
@@ -21,42 +71,7 @@ const Tooltip: FC<Props> = ({
 }) => {
   return (
     <div
-      className={classnames(
-        'relative inline-block text-center before:pointer-events-none before:absolute before:z-[9999] before:w-max before:max-w-xs before:rounded before:bg-neutral-800 before:px-2 before:py-1 before:text-xs before:text-neutral-50 before:opacity-0 before:delay-100 before:duration-200 before:ease-in-out before:content-[attr(data-tip)] hover:before:opacity-100 hover:before:delay-75 dark:before:bg-neutral-700',
-        {
-          "after:absolute after:z-[9999] after:block after:h-0 after:w-0 after:border-[3px] after:border-transparent after:opacity-0 after:delay-100 after:duration-200 after:ease-in-out after:content-[''] hover:after:opacity-100 hover:after:delay-75":
-            arrow,
-
-          'before:bottom-[calc(100%+4px)] before:top-auto': position === 'top',
-          'before:left-[calc(100%+5px)]': position === 'right',
-          'before:top-[calc(100%+4px)]': position === 'bottom',
-          'before:left-auto before:right-[calc(100%+5px)]': position === 'left',
-
-          'before:left-1/2 before:-translate-x-1/2':
-            position === 'top' || position === 'bottom',
-          'before:right-auto': position !== 'left',
-          'before:top-1/2 before:-translate-y-1/2':
-            position === 'right' || position === 'left',
-          'before:bottom-auto': position !== 'top',
-
-          'after:bottom-[calc(100%-2px)] after:top-auto after:border-t-neutral-800 dark:after:border-t-neutral-700':
-            position === 'top' && arrow,
-          'after:left-[calc(100%-1px)] after:border-r-neutral-800 dark:after:border-r-neutral-700':
-            position === 'right' && arrow,
-          'after:top-[calc(100%-2px)] after:border-b-neutral-800 dark:after:border-b-neutral-700':
-            position === 'bottom' && arrow,
-          'after:left-auto after:right-[calc(100%-1px)] after:border-l-neutral-800 dark:after:border-l-neutral-700':
-            position === 'left' && arrow,
-
-          'after:left-1/2 after:-translate-x-1/2':
-            (position === 'top' || position === 'bottom') && arrow,
-          'after:right-auto': position !== 'left' && arrow,
-          'after:top-1/2 after:-translate-y-1/2':
-            (position === 'right' || position === 'left') && arrow,
-          'after:bottom-auto': position !== 'top' && arrow
-        },
-        className
-      )}
+      className={variants({ position, arrow, className })}
       data-tip={content}
       role="tooltip"
     >
