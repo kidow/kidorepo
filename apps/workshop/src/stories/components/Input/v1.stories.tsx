6bf6f5cc8483465eec75cf578105c09702f8da91
kidow <wcgo2ling@gmail.com>
2023-08-09 21:10:48 +0900

    feat(workshop): input component

--- apps/workshop/src/stories/components/Input/v1.stories.tsx
+++ apps/workshop/src/stories/components/Input/v1.stories.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
 import { Input } from 'ui'
 
@@ -5,9 +6,42 @@ export default {
   title: 'components/Input/v1',
   tags: ['autodocs'],
   component: Input.v1,
-  argTypes: {}
+  argTypes: {
+    suffix: {
+      type: 'symbol'
+    },
+    required: {
+      type: 'boolean'
+    },
+    placeholder: {
+      type: 'string'
+    },
+    fullWidth: {
+      type: 'boolean'
+    },
+    float: {
+      type: 'boolean'
+    },
+    className: {
+      type: 'string'
+    }
+  }
 } satisfies Meta<typeof Input.v1>
 
 type Story = StoryObj<typeof Input.v1>
 
-export const Default: Story = {}
+function Example() {
+  const [value, setValue] = useState<string>('')
+  return (
+    <Input.v1
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      placeholder="Label"
+      float
+    />
+  )
+}
+
+export const Default: Story = {
+  render: () => <Example />
+}
--- packages/ui/components/Input/v1.tsx
+++ packages/ui/components/Input/v1.tsx
@@ -1,5 +1,78 @@
-function Input() {
-  return <></>
+'use client'
+
+import { useId } from 'react'
+import type {
+  ChangeEventHandler,
+  FC,
+  HTMLInputTypeAttribute,
+  KeyboardEvent,
+  ReactNode
+} from 'react'
+import { cn } from 'utils'
+
+export interface Props {
+  suffix?: ReactNode
+  required?: boolean
+  type?: HTMLInputTypeAttribute
+  placeholder?: string
+  fullWidth?: boolean
+  float?: boolean
+  className?: string
+  value?: string
+  onChange?: ChangeEventHandler<HTMLInputElement>
+  onEnter?: () => void
+}
+
+const Input: FC<Props> = ({
+  required,
+  type,
+  suffix,
+  placeholder,
+  fullWidth,
+  float,
+  className,
+  value,
+  onChange,
+  onEnter
+}) => {
+  const id = useId()
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !!onEnter) onEnter()
+  }
+  return (
+    <div className={cn('inline-block', { 'w-full': fullWidth }, className)}>
+      <div
+        className={cn(
+          'ring-primary focus:border-primary rounded border border-neutral-200 px-3 py-2 duration-150 focus-within:ring',
+          { 'w-full': fullWidth, relative: !!placeholder }
+        )}
+      >
+        <input
+          className={cn('w-full bg-transparent focus:outline-none', {
+            'peer placeholder:text-transparent': !!placeholder && float
+          })}
+          id={id}
+          type={type}
+          onKeyDown={onKeyDown}
+          onChange={onChange}
+          value={value}
+          required={required}
+          autoComplete="off"
+          placeholder={placeholder}
+          spellCheck={false}
+        />
+        <label className="absolute -top-6 left-0 max-w-[calc(100%-24px)] cursor-text select-none truncate text-sm text-slate-600 transition-all peer-placeholder-shown:left-3 peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-slate-400 peer-focus:-top-6 peer-focus:left-0 peer-focus:max-w-full peer-focus:cursor-default peer-focus:text-sm peer-focus:text-slate-400">
+          {placeholder}
+        </label>
+        {suffix}
+      </div>
+    </div>
+  )
+}
+
+Input.defaultProps = {
+  type: 'text',
+  float: true
 }
 
 export default Input
