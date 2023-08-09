afa0cb8829d8b98b9ac207f11693e21f1ef791f5
kidow <wcgo2ling@gmail.com>
2023-08-09 15:16:06 +0900

    feat(workshop): count-to component

--- apps/workshop/src/stories/components/CountTo/v1.stories.tsx
+++ apps/workshop/src/stories/components/CountTo/v1.stories.tsx
@@ -5,9 +5,30 @@ export default {
   title: 'components/CountTo/v1',
   tags: ['autodocs'],
   component: CountTo.v1,
-  argTypes: {}
+  argTypes: {
+    from: {
+      type: 'number',
+      defaultValue: 0
+    },
+    to: {
+      type: 'number'
+    },
+    duration: {
+      type: 'number',
+      defaultValue: 2000
+    },
+    className: {
+      type: 'string'
+    }
+  }
 } satisfies Meta<typeof CountTo.v1>
 
 type Story = StoryObj<typeof CountTo.v1>
 
-export const Default: Story = {}
+export const Default: Story = {
+  args: {
+    from: 0,
+    to: 3000,
+    duration: 2000
+  }
+}
--- packages/ui/components/CountTo/v1.tsx
+++ packages/ui/components/CountTo/v1.tsx
@@ -1,5 +1,36 @@
-function CountTo() {
-  return <></>
+import { memo, useEffect, useRef, useState } from 'react'
+
+export interface Props {
+  from?: number
+  to: number
+  duration?: number
+  className?: string
 }
 
-export default CountTo
+function CountTo({ from = 0, to, duration = 2000, className }: Props) {
+  const [count, setCount] = useState(from)
+
+  const startTimestamp = useRef<number>(0)
+
+  useEffect(() => {
+    const step = (timestamp: number) => {
+      if (!startTimestamp.current) {
+        startTimestamp.current = timestamp
+      }
+      const progress = Math.floor(timestamp - startTimestamp.current)
+      const increment = ((to - from) * progress) / duration
+      if (progress < duration) {
+        window.requestAnimationFrame(step)
+      }
+      if (to < Math.floor(from + increment)) {
+        setCount(to)
+      } else {
+        setCount(Math.floor(from + increment))
+      }
+    }
+    window.requestAnimationFrame(step)
+  }, [from, to, duration])
+  return <span className={className}>{count.toLocaleString()}</span>
+}
+
+export default memo(CountTo)
