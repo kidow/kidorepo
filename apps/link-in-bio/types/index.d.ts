66db3dd5d832088b17abb86dc7580bae7f6deed0
kidow <wcgo2ling@gmail.com>
2023-08-11 15:19:05 +0900

    refactor(link-in-bio): ntoast type 참조 에러 수정

--- apps/link-in-bio/types/index.d.ts
+++ apps/link-in-bio/types/index.d.ts
@@ -111,3 +111,16 @@ interface ModalProps {
   padding?: boolean
   footer?: ReactNode
 }
+
+namespace NToast {
+  type Type = 'success' | 'info' | 'warn' | 'error'
+  interface Emit {
+    message: string
+    type: NToast.Type
+  }
+  interface Item {
+    id: string
+    message: string
+    type: NToast.Type
+  }
+}
