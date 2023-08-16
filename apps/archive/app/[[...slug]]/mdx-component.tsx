ce68ca1635addf56dfe89b10a171b7bd37cb8e86
kidow <wcgo2ling@gmail.com>
2023-08-16 20:08:25 +0900

    fix(archive): modal block error 해결

--- apps/archive/app/[[...slug]]/mdx-component.tsx
+++ apps/archive/app/[[...slug]]/mdx-component.tsx
@@ -1,17 +1,10 @@
 'use client'
 
-import {
-  useEffect,
-  useMemo,
-  useState,
-  type HTMLAttributes,
-  type ImgHTMLAttributes
-} from 'react'
-import { allContents } from '@/.contentlayer/generated'
+import type { HTMLAttributes, ImgHTMLAttributes } from 'react'
+import Link from 'next/link'
 import { Icon } from '@/components'
 import { BoxIcon, CopyIcon } from 'lucide-react'
 import { useMDXComponent } from 'next-contentlayer/hooks'
-import { Modal } from 'ui'
 import { cn, toast } from 'utils'
 
 interface Props {
@@ -171,66 +164,39 @@ const components = {
       {children}
     </div>
   ),
-  ModalBlock: ({
+  LinkBlock: ({
+    slug,
     icon,
-    name,
-    slug
+    name
   }: {
+    slug: string
     icon: 'components' | 'hooks' | 'utils'
     name: string
-    slug: string
-  }) => {
-    const [isOpen, setIsOpen] = useState(false)
-    const doc = useMemo(
-      () => allContents.find((doc) => doc.slug === slug),
-      [slug]
-    )
-    useEffect(() => {
-      if (isOpen) {
-        document.body.style.overflow = 'hidden'
-      } else if (document.body.style.overflow === 'hidden') {
-        document.body.style.removeProperty('overflow')
-      }
-    }, [isOpen])
-    return (
-      <>
-        <button
-          className="group flex items-center justify-start gap-2 rounded-lg border border-gray-200 bg-transparent p-4 text-gray-700 shadow-sm shadow-gray-100 transition-all duration-200 hover:border-gray-300 hover:bg-slate-50 hover:text-gray-900 hover:shadow-md hover:shadow-gray-100 active:shadow-sm active:shadow-gray-200 dark:border-neutral-800 dark:text-neutral-200 dark:shadow-none dark:hover:border-neutral-700 dark:hover:bg-neutral-900 dark:hover:text-neutral-50 dark:hover:shadow-none"
-          onClick={() => setIsOpen(true)}
-        >
-          {icon === 'components' && (
-            <BoxIcon className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
-          )}
-          {icon === 'hooks' && (
-            <Icon.Hook className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
-          )}
-          {icon === 'utils' && (
-            <Icon.Wrench className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
-          )}
-          <span className="font-semibold">{name}</span>
-        </button>
-        <Modal.v1
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false)}
-          maxWidth="max-w-4xl"
-        >
-          {doc && (
-            <article>
-              <h1 className="text-4xl font-bold tracking-tight">{doc.title}</h1>
-              <MDXComponent code={doc.body.code} />
-            </article>
-          )}
-        </Modal.v1>
-      </>
-    )
-  },
+  }) => (
+    <Link
+      className="group flex items-center justify-start gap-2 rounded-lg border border-gray-200 bg-transparent p-4 text-gray-700 shadow-sm shadow-gray-100 transition-all duration-200 hover:border-gray-300 hover:bg-slate-50 hover:text-gray-900 hover:shadow-md hover:shadow-gray-100 active:shadow-sm active:shadow-gray-200 dark:border-neutral-800 dark:text-neutral-200 dark:shadow-none dark:hover:border-neutral-700 dark:hover:bg-neutral-900 dark:hover:text-neutral-50 dark:hover:shadow-none"
+      href={slug}
+    >
+      {icon === 'components' && (
+        <BoxIcon className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
+      )}
+      {icon === 'hooks' && (
+        <Icon.Hook className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
+      )}
+      {icon === 'utils' && (
+        <Icon.Wrench className="h-6 w-6 text-neutral-400 duration-200 group-hover:text-neutral-600 dark:text-neutral-600 dark:group-hover:text-neutral-50" />
+      )}
+      <span className="font-semibold">{name}</span>
+    </Link>
+  ),
   Storybook: ({ url }: { url: string }) => (
     <>
       <iframe
-        src={url}
+        src={`${url}&viewMode=story&shortcuts=false&singleStory=true&full=1`}
         className="mt-6 hidden md:block"
         width="800"
         height="440"
+        allowFullScreen
       />
       <p className="mt-2 md:hidden">It can only be viewed in a mobile.</p>
     </>
